941cb7976de393c5a1e202341615769b6a654ab3
TheLunarSociety <137660579+TheLunarSociety@users.noreply.github.com>
2026-01-06 22:20:12 +0800

    Create DDI_BootingDriver.h

---
+++ 1.0/Toshiba/DISP_FCT_JIG/trunk/00.Code/4.Driver/Include/DDI_BootingDriver.h
@@ -0,0 +1,21 @@
+/*****************************************************************************
+	DD Type(BLDCM) Washing Machine Program
+	Model					: Panacea-PJT
+	Version					: Ver 1.0
+	CPU						: MN103SFA5K
+	File type				: C header file
+	File name				: DDI_BootingDriver.h
+	Included Funtion		:
+
+	be coded by KIM KYUNG HOON
+********************************************************************************/
+#include "../../4.Driver/Include/WROS_Register.h"
+
+#include "../../5.Hardware/Include/m470_tmrb.h"
+#include "../../5.Hardware/Include/m470_int.h"
+#include "../../5.Hardware/Include/m470_sio.h"
+#include "../../5.Hardware/Include/m470_port.h"
+#include "../../5.Hardware/Include/m470_wdt.h"
+
+void DDI_WROS_BootMicom(void);
+
